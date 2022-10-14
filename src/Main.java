0e49bf0c02ea3684606889e4cc9a76f5d64c39a5
SantiagoVira <santiagovira06@gmail.com>
2022-10-14 11:51:22 -0400

    Code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CustomStringMethods methods = new CustomStringMethods();
+
+        String str = methods.removeCharacter("Halloween", 5);
+        System.out.println(str);
+        str = methods.removeCharacter("Halloween", 0);
+        System.out.println(str);
+        str = methods.removeCharacter("Halloween", 8);
+        System.out.println(str);
+        str = methods.removeCharacter("Halloween", 9);
+        System.out.println(str);
+        str = methods.removeCharacter("Halloween", 20);
+        System.out.println(str);
+        str = methods.removeCharacter("Hi friend", 0);
+        System.out.println(str);
+        str = methods.removeCharacter("Hi friend", 1);
+        System.out.println(str);
+        str = methods.removeCharacter("Hi friend", 2);
+        System.out.println(str);
+        str = methods.removeCharacter(" Hi friend", 0);
+        System.out.println(str);
+        str = methods.removeCharacter("A", 0);
+        System.out.println(str);
+        str = methods.removeCharacter("A ", 0);
+        System.out.println(str);
+        str = methods.removeCharacter("A", 1);
+        System.out.println(str);
+        str = methods.removeCharacter("A ", 1);
+        System.out.println(str);
+
+
     }
 }
